3dec58549bbb3dda0717d8a34802132c1f307119
MelAlejandrino <alejandrinomel02@gmail.com>
2026-03-13 16:12:27 +0800

    beta: data-table

--- index.mjs
+++ index.mjs
@@ -1,32 +1,33 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import { execa } from "execa";
+import {program} from "commander";
+import {execa} from "execa";
 
 const REGISTRY_BASE = "https://jeje-ui.vercel.app/r";
 
 const components = {
-  autocomplete: `${REGISTRY_BASE}/autocomplete.json`,
-  "image-uploader": `${REGISTRY_BASE}/image-uploader.json`,
-  "virtualized-dropdown": `${REGISTRY_BASE}/virtualized-dropdown.json`,
+    autocomplete: `${REGISTRY_BASE}/autocomplete.json`,
+    "image-uploader": `${REGISTRY_BASE}/image-uploader.json`,
+    "virtualized-dropdown": `${REGISTRY_BASE}/virtualized-dropdown.json`,
+    "data-grid": `${REGISTRY_BASE}/data-grid.json`,
 };
 
 program.name("jejeui").description("jeje/ui component CLI").version("1.0.0");
 
 program
-  .command("add <component>")
-  .description("Add a jeje/ui component to your project")
-  .action(async (component) => {
-    const url = components[component];
+    .command("add <component>")
+    .description("Add a jeje/ui component to your project")
+    .action(async (component) => {
+        const url = components[component];
 
-    if (!url) {
-      console.error(`Unknown component: "${component}"`);
-      console.log(`\nAvailable components:`);
-      Object.keys(components).forEach((c) => console.log(`  - ${c}`));
-      process.exit(1);
-    }
+        if (!url) {
+            console.error(`Unknown component: "${component}"`);
+            console.log(`\nAvailable components:`);
+            Object.keys(components).forEach((c) => console.log(`  - ${c}`));
+            process.exit(1);
+        }
 
-    console.log(`Installing ${component}...`);
-    await execa("npx", ["shadcn@latest", "add", url], { stdio: "inherit" });
-  });
+        console.log(`Installing ${component}...`);
+        await execa("npx", ["shadcn@latest", "add", url], {stdio: "inherit"});
+    });
 
 program.parse();
